fdc68a41b2b0ee93617867df1e9dfd93fd003ae0
EricTheRay <121706137+EricTheRay@users.noreply.github.com>
2022-12-31 17:11:22 +0800

    updated

--- CountDown/index.js
+++ CountDown/index.js
@@ -1,15 +1,34 @@
-﻿let h1 = document.getElementById("h1");
-let h2 = document.getElementById("h2");
+﻿let text1 = document.getElementById("text1");
+let text2 = document.getElementById("text2");
 let clock = document.getElementById("clock");
 
-h1.style.fontSize = "32px";
-h2.style.fontSize = "18px";
+text1.style.fontSize = "32px";
+text2.style.fontSize = "20px";
 clock.style.fontSize = "18px";
+
+let newYearDate = new Date(2023, 0, 1, 0, 0, 0, 0);
+
+pageInitialize();
+
+function pageInitialize()
+{
+    let currentDate = new Date();
+    let countDown = newYearDate.getTime() - currentDate.getTime();
+    let countDownDate = new Date(countDown);
+    let h = countDownDate.getHours() - 8;
+    let m = countDownDate.getMinutes();
+    let s = countDownDate.getSeconds();
+
+    text1.textContent = "前進 2023";
+    text2.textContent = "跨年倒數";
+    clock.textContent = ("0" + h).substr(-2) + ":" + ("0" + m).substr(-2) + ":" + ("0" + s).substr(-2);
+}
+
+let intervalID = setInterval(displayTime, 1000);
 
 function displayTime()
 {
     let currentDate = new Date();
-    let newYearDate = new Date(2023, 0, 1, 0, 0, 0, 0);
 
     if (currentDate <= newYearDate)
     {
@@ -23,13 +42,14 @@ function displayTime()
     }
     else
     {
-        h1.innerHTML = "";
-        h2.innerHTML = "";
-        clock.style.fontSize = "60px";
-        clock.textContent = "Happy New Year!!";
+        text1.style.fontSize = "5px";
+        text2.style.fontSize = "60px";
+        clock.style.fontSize = "5px";
+
+        text1.textContent = "";
+        text2.textContent = "Happy New Year!!";
+        clock.textContent = "";
+
+        clearInterval(intervalID);
     }
 }
-
-displayTime();
-
-setInterval(displayTime, 1000);
