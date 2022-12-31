e9a7f0406feeb96a13b98ce2896a87978849e428
EricTheRay <121706137+EricTheRay@users.noreply.github.com>
2022-12-31 17:50:47 +0800

    update

--- CountDown/index.js
+++ CountDown/index.js
@@ -2,10 +2,6 @@
 let text2 = document.getElementById("text2");
 let clock = document.getElementById("clock");
 
-text1.style.fontSize = "32px";
-text2.style.fontSize = "20px";
-clock.style.fontSize = "18px";
-
 let intervalID;
 
 let newYearDate = new Date(2023, 0, 1, 0, 0, 0, 0);
@@ -23,6 +19,10 @@ function pageInitialize()
         let h = countDownDate.getHours() - 8;
         let m = countDownDate.getMinutes();
         let s = countDownDate.getSeconds();
+
+        text1.style.fontSize = "32px";
+        text2.style.fontSize = "20px";
+        clock.style.fontSize = "18px";
 
         text1.textContent = "前進 2023";
         text2.textContent = "跨年倒數";
@@ -54,8 +54,6 @@ function displayTime()
         let m = countDownDate.getMinutes();
         let s = countDownDate.getSeconds();
 
-        text1.textContent = "前進 2023";
-        text2.textContent = "跨年倒數";
         clock.textContent = ("0" + h).substr(-2) + ":" + ("0" + m).substr(-2) + ":" + ("0" + s).substr(-2);
     }
     else
